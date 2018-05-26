bcc9ffc4598e3bd11ac058c588907d8203c7bc31
Victoria Earl <kitsuta@gmail.com>
2018-05-26 19:00:17 -0400

    Stop celery-beat from failing if you restart the container If you used docker-compose restart for the celery-beat container, it would think it's already running because it saw the old pidfile. That would prevent it from running the automated emails task ever again, so we fixed it.

--- uber-wrapper.sh
+++ uber-wrapper.sh
@@ -5,7 +5,7 @@ if [ "$1" = 'uber' ]; then
     /app/env/bin/python3 /app/sideboard/sep.py alembic upgrade heads
     /app/env/bin/python3 /app/sideboard/run_server.py
 elif [ "$1" = 'celery-beat' ]; then
-    /app/env/bin/celery -A uber.tasks beat
+    /app/env/bin/celery -A uber.tasks beat --pidfile=
 elif [ "$1" = 'celery-worker' ]; then
     /app/env/bin/celery -A uber.tasks worker
 fi
